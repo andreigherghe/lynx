0c2c6dedbf2d065395b89b90abfe621c9e3419ac
andreigherghe <andreigherghe@me.com>
2018-07-06 01:50:17 +0300

    added library target

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Lynx",
+    products: [
+        .library(name: "Lynx", targets: ["Lynx"]),
+    ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
